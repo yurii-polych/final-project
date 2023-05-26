faaec4dc0e2539cb58cc8af871828edfc06f7efb
Yurii Polych <bighunt0@gmail.com>
2023-05-26 17:14:32 +0300

    add new logic for MessageHandler

--- tg_bot/models.py
+++ tg_bot/models.py
@@ -6,10 +6,10 @@ class UserModel(db.Model):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, unique=True, nullable=False)
-    first_name = db.Column(db.String)
-    last_name = db.Column(db.String)
-    username = db.Column(db.String)
-    is_searching = db.Column(db.Boolean, default=False)
+    first_name = db.Column(db.String(20))
+    last_name = db.Column(db.String(40))
+    username = db.Column(db.String(30))
+    last_message = db.Column(db.String(255), default=False)
     created = db.Column(db.DateTime, default=datetime.now())
 
 
