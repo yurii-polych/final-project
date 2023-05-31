bb2ecd40eb73e8560b6115a2e68655b9b667ea0c
Yurii Polych <bighunt0@gmail.com>
2023-05-31 22:56:49 +0300

    add comments

--- tg_bot/__init__.py
+++ tg_bot/__init__.py
@@ -3,11 +3,20 @@ from logging.config import dictConfig
 from .config import BotConfig, set_bot_commands, set_webhook
 from flask_sqlalchemy import SQLAlchemy
 
+# This code initializes a SQLAlchemy object, which is a toolkit and ORM (Object-Relational Mapping) for Python. 
+# It provides a set of high-level API to interact with relational databases.
 db = SQLAlchemy()
+    
 
 app = Flask(__name__)
 
 dictConfig({
+    """
+    This code sets up a logging configuration for a Flask application. 
+    It specifies a formatter that includes the time, log level, module name, and message. 
+    It also sets up a handler that logs to the Flask error stream. 
+    Finally, it sets the root logger level to INFO and assigns the handler to it.
+    """
     'version': 1,
     'formatters': {'default': {
         'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
@@ -24,11 +33,15 @@ dictConfig({
 })
 
 set_webhook()
+
 set_bot_commands()
 
+# Load the configuration settings for the Flask app from the BotConfig object.
 app.config.from_object(BotConfig)
 
+
 db.init_app(app)
+
 
 from .views import *
 from .models import *
--- tg_bot/views.py
+++ tg_bot/views.py
@@ -10,6 +10,11 @@ from .handlers import MessageHandler, CallbackHandler
 
 @app.route('/', methods=["POST"])
 def hello():
+    """
+    This is a Flask route that listens for incoming POST requests. If the request contains a JSON object with a "message" key, 
+    it creates a MessageHandler object and calls its handle() method. If the request contains a JSON object with a "callback_query" key, 
+    it creates a CallbackHandler object and calls its handle() method. In either case, it returns the string "Ok".
+    """
     # pprint(f'First print in route: {datetime.datetime.now()}')
     if message := request.json.get('message'):
         # pprint(request.json)
